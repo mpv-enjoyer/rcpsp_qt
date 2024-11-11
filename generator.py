de43c461509c6267595fe5af7150ace0231f0f0f
Alex <Disblocker.just@gmail.com>
2024-11-11 16:52:25 +0300

    generator concept ready?

--- generator.py
+++ generator.py
@@ -7,14 +7,29 @@
 
 import random
 
-# job count 10 ~ 10000
+## whatever dist - dist that is defined only for that line and is randomized
+## dependant dist - whatever dist that can be changed according to the previous value
 
-target_ancestors_distribution = random.uniform(0.0, 1.0)
-# loop through every ancestor and add that ancestor if random_change(target_ancestors_distribution) is true
-# also we can use normal distribution around that value from time to time
-
-target_job_area_busyness_distribution = random.uniform(0, 1)
-
+# job_count = uniform dist 10 ~ 10000
+# max_plan_loop = uniform dist 10000 ~ 50000
+# current_plan_section_count: N = δ_dist (LIMIT 0 < N <= 100) * 2
+# current_plan_current_section_time = dependant dist 1 ~ ( max_plan_loop / current_plan_section_count )
+#
+#
+# max_plan_unit = MAX(current_plan_unit...)
+# min_job_time_to_spend, max_job_time_to_spend = MIN, MAX from: uniform dist 1 ~ max_plan_unit, uniform dist 1 ~ max_plan_unit
+# current_job_time_to_spend = whatever dist min_job_time_to_spend ~ max_job_time_to_spend
+# current_job_busyness_section_count = whatever dist 1 ~ current_job_time_to_spend / 2
+# current_job_current_busyness_value = dependant dist 0 ~ 1
+# max_job_group_count = uniform dist 1 ~ job_count
+# current_job_group = whatever dist 1 ~ max_job_group_count
+# job_group_count = clamp unused job_groups
+# max_worker_group_count = uniform dist 1 ~ job_group_count
+# current_worker's_group_id = whatever dist 1 ~ max_worker_group_count
+# worker_group_count = clamp unused worker_groups
+# current_job_group_corresponds_to_N_worker_groups: N = δ_dist (LIMIT 0 < N <= worker_group_count)
+# current_job_group_to_id_N_worker_group: N = uniform dist 1 ~ worker_group_count
+# current_job_ancestors_count = dependant dist 0 ~ possible_ancestors_left
 
 file = open('generated_sample.txt', 'w+')
 line = " "
