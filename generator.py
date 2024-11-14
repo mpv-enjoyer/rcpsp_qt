0bc294f2fce47c68aef7a97fe691315540eb8737
Alex <Disblocker.just@gmail.com>
2024-11-14 20:15:25 +0300

    optimized dependant_dist, file write etc.

--- generator.py
+++ generator.py
@@ -41,7 +41,7 @@ ROUND = 3
 def get_truncated_normal(mean=0.0, sd=1.0, low=0.0, upp=10.0, size=1):
     if size == 1:
         return round(truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd).rvs(), ROUND)
-    return round(truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd).rvs(size), ROUND)
+    return [round(i, ROUND) for i in truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd).rvs(size)]
 
 def get_random_whatever_dist() -> str:
     return rng.choice(['trnorm', 'uniform'])
@@ -65,37 +65,35 @@ def whatever_dist_int(low: int, high: int, size = 1, dist = 'none'):
         mean = get_random_int(low, high)
         if size == 1:
             return int(get_truncated_normal(mean, 1, low, high))
-        return map(int, get_truncated_normal(mean, 1, low, high, size))
+        return list(map(int, get_truncated_normal(mean, 1, low, high, size)))
     if dist == 'uniform':
         if size == 1:
             return get_random_int(low, high)
         return get_random_int(low, high, size)
-    raise "unvalid dist"
+    raise "invalid dist"
 
 def dependant_dist_int(low: int, high: int, size: int, dist = 'none'):
-    output = []
-    if low == high:
-        high += 1
     if dist == 'none':
         dist = get_random_dependant_dist()
-    for i in range(size):
-        if dist == 'uniform_asc':
-            section_size = float((high - low) / size)
-            output.append(get_random_int(low + section_size * i, low + section_size * (i + 1)))
-        if dist == 'uniform_desc':
-            rev_i = size - i - 1
-            section_size = float((high - low) / size)
-            output.append(get_random_int(low + section_size * rev_i, low + section_size * (rev_i + 1)))
-        if dist == 'uniform_back_and_forth':
+    section_size = float((high - low) / size)
+    if dist == 'uniform_asc':
+        return [get_random_int(low + section_size * i, low + section_size * (i + 1)) for i in range(size)]
+    if dist == 'uniform_desc':
+        #rev_i = size - i - 1
+        return [get_random_int(low + section_size * (size - i - 1), low + section_size * (size - i)) for i in range(size)]
+    if dist == 'trnorm' or dist == 'uniform':
+        return whatever_dist_int(low, high, size, dist)
+    output = [0] * size
+    if dist == 'uniform_back_and_forth':
+        for i in range(size):
             if i == 0:
-                output.append(get_random_int(low, high))
+                output[0] = get_random_int(low, high)
             elif i % 2 == 0:
-                output.append(get_random_int(output[-1], high))
+                output[i] = get_random_int(output[i - 1], high)
             else:
-                output.append(get_random_int(low, output[-1]))
-        if dist == 'trnorm' or dist == 'uniform':
-            output.append(whatever_dist_int(low, high, 1, dist))
-    return output
+                output[i] = get_random_int(low, output[i - 1])
+        return output
+    raise "invalid dist"
 
 def get_random_float(low: float, high: float, size = 1) -> float:
     if size == 1:
@@ -109,35 +107,35 @@ def whatever_dist_float(low: float, high: float, size = 1, dist = 'none'):
         mean = get_random_float(low, high)
         if size == 1:
             return get_truncated_normal(mean, 1, low, high)
-        return map(float, get_truncated_normal(mean, 1, low, high, size))
+        return list(map(float, get_truncated_normal(mean, 1, low, high, size)))
     if dist == 'uniform':
         if size == 1:
             return get_random_float(low, high)
         return get_random_float(low, high, size)
-    raise "unvalid dist"
+    raise "invalid dist"
 
 def dependant_dist_float(low: float, high: float, size: int, dist = 'none'):
-    output = []
     if dist == 'none':
         dist = get_random_dependant_dist()
-    for i in range(size):
-        if dist == 'uniform_asc':
-            section_size = float((high - low) / size)
-            output.append(get_random_float(low + section_size * i, low + section_size * (i + 1)))
-        if dist == 'uniform_desc':
-            rev_i = size - i - 1
-            section_size = float((high - low) / size)
-            output.append(get_random_float(low + section_size * rev_i, low + section_size * (rev_i + 1)))
-        if dist == 'uniform_back_and_forth':
+    section_size = float((high - low) / size)
+    if dist == 'uniform_asc':
+        return [get_random_float(low + section_size * i, low + section_size * (i + 1)) for i in range(size)]
+    if dist == 'uniform_desc':
+        #rev_i = size - i - 1
+        return [get_random_float(low + section_size * (size - i - 1), low + section_size * (size - i)) for i in range(size)]
+    if dist == 'trnorm' or dist == 'uniform':
+        return whatever_dist_float(low, high, size, dist)
+    output = [0.0] * size
+    if dist == 'uniform_back_and_forth':
+        for i in range(size):
             if i == 0:
-                output.append(get_random_float(low, high))
+                output[0] = get_random_float(low, high)
             elif i % 2 == 0:
-                output.append(get_random_float(output[-1], high))
+                output[i] = get_random_float(output[i - 1], high)
             else:
-                output.append(get_random_float(low, output[-1]))
-        if dist == 'trnorm' or dist == 'uniform':
-            output.append(whatever_dist_float(low, high, 1, dist))
-    return output
+                output[i] = get_random_float(low, output[i - 1])
+        return output
+    raise "invalid dist"
 
 # Begin generation:
 generated = ""
@@ -152,10 +150,13 @@ for plan in range(plan_count):
     else:
         current_plan_start_at = get_random_int(0, max_plan_loop / 100)
     generated += "plan;" + str(plan) + ";" + str(current_plan_start_at) + ";"
-    work = dependant_dist_int(1, max_plan_loop / current_plan_unit_count, current_plan_unit_count)
-    rest = dependant_dist_int(1, max_plan_loop / current_plan_unit_count, current_plan_unit_count)
-    for unit in range(current_plan_unit_count):
-        generated += str(work[unit]) + ";" + str(rest[unit]) + ";"
+    work = dependant_dist_int(1, max(max_plan_loop / current_plan_unit_count, 1), current_plan_unit_count)
+    rest = dependant_dist_int(1, max(max_plan_loop / current_plan_unit_count, 1), current_plan_unit_count)
+    if current_plan_unit_count == 1:
+        generated += str(work) + ";" + str(rest) + ";"
+    else:
+        for unit in range(current_plan_unit_count):
+            generated += str(work[unit]) + ";" + str(rest[unit]) + ";"
     generated += "\n"
 max_plan_unit = max(work)
 min_job_time_to_spend = get_random_int(1, max_plan_unit)
@@ -172,19 +173,26 @@ job_groups = set()
 
 import timeit
 
+f = open("generated_sample.txt", "w+")
+f.write(generated)
+generated = ""
+
 for job in range(job_count):
     jobs_left_to_iterate = job_count - 1 - job
     current_job_max_time_to_spend = whatever_dist_int(min_job_time_to_spend, max_job_time_to_spend, 1, wdist1)
-    print(timeit.timeit(lambda: whatever_dist_int(min_job_time_to_spend, max_job_time_to_spend, 1, wdist1), number = 1000))
+    #print(timeit.timeit(lambda: whatever_dist_int(min_job_time_to_spend, max_job_time_to_spend, 1, wdist1), number = 1000))
     current_job_busyness_section_count = whatever_dist_int(1, current_job_max_time_to_spend / 2, 1, wdist2)
-    print(timeit.timeit(lambda: whatever_dist_int(1, current_job_max_time_to_spend / 2, 1, wdist2), number = 1000))
+    #print(timeit.timeit(lambda: whatever_dist_int(1, current_job_max_time_to_spend / 2, 1, wdist2), number = 1000))
     current_job_busyness_values = dependant_dist_float(0, 1, current_job_busyness_section_count)
-    print(timeit.timeit(lambda: dependant_dist_float(0, 1, current_job_busyness_section_count), number = 100))
+    #print(timeit.timeit(lambda: dependant_dist_float(0, 1, current_job_busyness_section_count), number = 1000))
     current_job_busyness_times = get_random_int(1, current_job_max_time_to_spend / current_job_busyness_section_count, current_job_busyness_section_count)
-    print(timeit.timeit(lambda: get_random_int(1, current_job_max_time_to_spend / current_job_busyness_section_count, current_job_busyness_section_count), number = 1000))
+    #print(timeit.timeit(lambda: get_random_int(1, current_job_max_time_to_spend / current_job_busyness_section_count, current_job_busyness_section_count), number = 1000))
     current_job_ancestors_count = whatever_dist_int(0, jobs_left_to_iterate, 1, wdist4)
+    #print(timeit.timeit(lambda: whatever_dist_int(0, jobs_left_to_iterate, 1, wdist4), number = 1000))
     current_job_ancestors = [(i + job + 1) for i in rng.choice(jobs_left_to_iterate, size=current_job_ancestors_count, replace=False)]
+    #print(timeit.timeit(lambda: [(i + job + 1) for i in rng.choice(jobs_left_to_iterate, size=current_job_ancestors_count, replace=False)], number = 1000))
     current_job_group = whatever_dist_int(0, max_job_group_count - 1, 1, wdist3)
+    #print(timeit.timeit(lambda: whatever_dist_int(0, max_job_group_count - 1, 1, wdist3), number = 1000))
     job_groups.add(current_job_group)
     generated += "job;" + str(job) + ";"
     if current_job_busyness_section_count == 1:
@@ -196,11 +204,14 @@ for job in range(job_count):
     for ancestor in current_job_ancestors:
         generated += str(ancestor) + ";"
     generated += "\n"
+    if job % 50 == 0:
+        f.write(generated)
+        generated = ""
     print(job / job_count * 100, "%")
 job_group_count = len(job_groups)
 print(job_groups)
 
 print(generated)
-f = open("generated_sample.txt", "w+")
+#f = open("generated_sample.txt", "w+")
 f.write(generated)
 f.close()
